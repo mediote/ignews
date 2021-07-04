8efaeb6acd8e665a422ab387173deab46f1d6fd3
mediote <mediote90@gmail.com>
2021-07-04 18:34:55 -0300

    Fix image tag

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,14 @@
 import { SignInButton } from '../SignInButton'
 import styles from './styles.module.scss'
+import Image from 'next/image'
+import logoImg from '../../public/images/logo.svg'
+
 
 export function Header(){
   return(
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <img src="/images/logo.svg" alt="ig.news" />
+        <Image src={logoImg} alt="ig.news" />
         <nav>
           <a className={styles.active}>Home</a>
           <a>Posts</a> 
