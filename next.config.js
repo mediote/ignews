317c3df2c9b825b620df949d1fd8220c2e7de960
mediote <mediote90@gmail.com>
2021-07-07 18:18:52 -0300

    Login with github by next-auth

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   env: {
     STRIPE_API_KEY: process.env.STRIPE_API_KEY,
+    GITHUB_ID: process.env.GITHUB_ID,
+    GITHUB_SECRET: process.env.GITHUB_SECRET,
   },
   reactStrictMode: true,
 }
